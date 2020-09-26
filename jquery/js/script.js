505c77e04be83e790c9ff2f303bf7b2edd88784f
lorencsejdaras <43918041+lorencsejdaras@users.noreply.github.com>
2020-09-26 15:31:11 +0200

    edited

---
+++ jquery/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+/*
+$('body h1:nth-child(1)').mouseenter(function () {
+
+  $(this).hide();
+
+})
+
+
+*/
+
+$("#button").click(function(){
+
+$('#epara').fadeOut();
+$('#edyta').hide().fadeIn(2000);
+$('#etreta').fadeOut("slow");
+})
+
+
+});
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 var books = [
   {
     title: "Harry Potter and the chamber of secrets",
@@ -10,8 +11,10 @@ var books = [
   author: "Ernest Koliqi",
   alreadyRead : true,
   image: "https://arratisje.al/wp-content/uploads/2019/10/ernest-koliqi.jpg"
-  }
+}
 ];
+
+
 
 /*
 for(var i=0; i<books.length; i++){
@@ -22,17 +25,19 @@ for(var i=0; i<books.length; i++){
   document.body.appendChild(p);
 }
 */
-var bookList = document.createElement('ul');
-for(var i = 0; i< books.length; i++){
-  var item = document.createElement('li');
-  var bImage = document.createElement('img');
-  bImage.src = books[i].image;
-  item.appendChild(bImage);
-  var node = document.createTextNode(books[i].title + ' by '+ books[i].author +  '.');
-  item.appendChild(node);
-  if(!books[i].alreadyRead ){
-    item.style.color = "red";
+
+
+  var bookList = document.createElement('ul');
+  for(var i = 0; i< books.length; i++){
+    var item = document.createElement('li');
+    var bImage = document.createElement('img');
+    bImage.src = books[i].image;
+    item.appendChild(bImage);
+    var node = document.createTextNode(books[i].title + ' by '+ books[i].author +  '.');
+    item.appendChild(node);
+    if(!books[i].alreadyRead ){
+      item.style.color = "red";
+    }
+    bookList.appendChild(item);
   }
-  bookList.appendChild(item);
-}
 document.body.appendChild(bookList);
